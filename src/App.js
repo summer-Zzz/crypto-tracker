acb517624c78ead57d0133ad74f468c6ee15b8ba
summer-Zzz <summerellaooo@gmail.com>
2021-05-09 01:20:59 +0800

    setting up front-end routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 
 import Chart from './components/Chart'
@@ -8,8 +13,37 @@ import './App.css';
 import { formatPrefix } from 'd3-format';
 
 function App() {
+  const [number, setNumber] = useState(0)
   return (
-    <Chart />
+    <Router>
+    <div className="App">
+      <header>
+        <h1 onClick={() => setNumber(number + 1)}>routing</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </nav>
+      </header>
+      <main>
+        <h2>Some other content</h2>
+        {/* {viewMode === "home" && <p>HOME</p>}
+        {viewMode === "login" && <p>LOGIN</p>}
+        {viewMode === "register" && <p>REGISTER</p>} */}
+        <Switch>
+          <Route path="/register">
+            <p>REGISTER</p>
+          </Route>
+          <Route path="/login">
+            <p>LOGIN</p>
+          </Route>
+          <Route path="/">
+            <p>HOME</p>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+    </Router>
   );
 }
 
