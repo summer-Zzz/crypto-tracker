0895138233ae0d20b552493403c90223318b660f
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-05-09 15:01:42 -0400

    add schema and seeds

--- express-server/db/schema/exchanges.sql
+++ express-server/db/schema/exchanges.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS exchanges CASCADE;
 CREATE TABLE exchanges(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
-  country_of_origin VARCHAR(100), 
   website VARCHAR(100),
 )
