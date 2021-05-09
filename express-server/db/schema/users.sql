52158d51190d2d61393e8065bda7f57fd88a05e6
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-05-09 10:18:19 -0400

    create users seed

--- express-server/db/schema/users.sql
+++ express-server/db/schema/users.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
-    username VARCHAR(255) NOT NULL,
     email VARCHAR(25e5) NOT NULL,
     password VARCHAR(255) NOT NULL,
     holdings INTEGER, 
---
+++ express-server/db/seeds/01_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+users(username, email, password)
+VALUES
+('jaredgoldman', 'password')
+('summerzhu', 'test@test.com', 'password')
+('emansawi', 'test@test.com', 'password')
+('nickman', 'test@test.com', 'password')
+('dogeboy', 'test@test.com', 'password')
+('somefuckingguy', 'test@test.com', 'password')
+('btcnerd', 'test@test.com', 'password')
+('cryptomooner', 'test@test.com', 'password')
