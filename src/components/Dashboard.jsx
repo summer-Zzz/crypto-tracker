a68691d52583a05935c438e3fd952bc3f2b63759
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-05-10 21:50:07 -0400

    basic dashboard skeleton created

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import DropMenu from './DropDownMenu'
 import InfoDisplay from './InfoDisplay'
+import TradeTable from './TradeTable'
+import CoinTable from './CoinTable'
 
-export default function Dashboard() {
+export default function Dashboard(props) {
 
   return (
-    <div>
-      <div className="chart"></div>
-      <div>
-        <DropMenu />
-        <DropMenu />
-        <DropMenu />
+    <div className='dashboard-container'>
+      <div className="chart-container">
+        <Chart />
       </div>
-      <InfoDisplay infoHeader={'Balance'} infoContent={'0.85452'}/>
-      <InfoDisplay infoHeader={'P%L'} infoContent={'+25%'}/>
+      <div className="menu-container">
+        <DropMenu options={props.exchanges}/>
+        <DropMenu options={props.timeframes}/>
+        <DropMenu options={props.currencies}/>
+      </div>
+      <div className="info-container">
+        <InfoDisplay infoHeader={'Balance'} infoContent={'0.85452'}/>
+        <InfoDisplay infoHeader={'P%L'} infoContent={'+25%'}/>
+      </div>
+      <div className="trade-container">
+        <TradeTable rows={props.TradeData}/>
+      </div>
+      <div className="coin-container">
+        <CoinTable rows={props.CoinData}/>
+      </div>
     </div>
   )
 
