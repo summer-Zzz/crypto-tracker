6c1b91dd64511a6cbed57e4661eecd91121b7240
summer-Zzz <summerellaooo@gmail.com>
2021-05-12 06:21:50 +0800

    fix chart styling

--- src/components/TradeTableRow.jsx
+++ src/components/TradeTableRow.jsx
@@ -3,12 +3,12 @@ import "../components/TradeTableRow.scss"
 export default function TradeTableRow(props) {
 
   return (
-    <tr>
-      <td>{props.tradeTime}</td>
-      <td>{props.tradeType}</td>
-      <td>{props.tradeOrder}</td>
-      <td>{props.tradePrice}</td>
-      <td>{props.tradeAmount}</td>
-    </tr>
+      <tr>
+        <td>{props.tradeTime}</td>
+        <td>{props.tradeType}</td>
+        <td>{props.tradeOrder}</td>
+        <td>{props.tradePrice}</td>
+        <td>{props.tradeAmount}</td>
+      </tr>
   )
 }
