a21537eb0a3f996621535cfe95a994c8d5233e9d
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-05-12 16:15:49 -0400

    reorganize component files

---
+++ src/components/TradeTable/TradeTable.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import TradeTableRow from "./TradeTableRow"
+import "./TradeTable.scss";
+
+export default function TradeTable(props) {
+
+  const tableRows = props.rows.map(row => {
+    return <TradeTableRow 
+      key={row.id}
+      tradeTime={row.tradeTime}
+      tradeType={row.tradeType}
+      tradeOrder={row.tradeOrder}
+      tradePrice={row.tradePrice}
+      tradeAmount={row.tradeAmount}
+    />
+
+  })
+  
+  return (
+  <table className="trade-table">
+      <thead>
+      <tr>
+        <td>Time</td>
+        <td>Type</td>
+        <td>Order %</td>
+        <td>Price</td>
+        <td>Amount</td>
+      </tr>
+    </thead>
+    <tbody>
+      {tableRows}
+    </tbody>
+  </table>
+  )
+  
+}
+
