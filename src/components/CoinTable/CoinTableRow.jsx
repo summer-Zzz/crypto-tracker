55ae5ea2869f418afaeda9f8672c58fd1c4e46b2
summer-Zzz <summerellaooo@gmail.com>
2021-05-18 06:30:58 +0800

    change dashboard title color

--- src/components/CoinTable/CoinTableRow.jsx
+++ src/components/CoinTable/CoinTableRow.jsx
@@ -10,7 +10,7 @@ export default function CoinTableRow(props) {
       <td className="price">{props.price}</td>
       <td className="coin-td">{props.change}</td>
       <td className="coin-td">{props.changePercent}</td>
-      <td className="coin-td">{props.volume}</td>
+      <td className="coin-td">{props.volume.toFixed(2)}</td>
       <td><button className="coin-button" onClick={() => props.setCoin(props.symbol)}>Select Coin</button></td>
     </tr>
   )
