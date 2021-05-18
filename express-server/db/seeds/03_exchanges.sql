1f6a6f2c02546ca8d11a7d89daf241386d601da1
summer-Zzz <summerellaooo@gmail.com>
2021-05-19 02:06:48 +0800

    change website in seeds

--- express-server/db/seeds/03_exchanges.sql
+++ express-server/db/seeds/03_exchanges.sql
@@ -5,6 +5,6 @@ VALUES
 ('bitmex', 'https://www.bitmex.com'),
 ('binance', 'https://www.binance.com/%27'),
 ('aax', 'https://www.aaxpro.com/'),
-('bitbuy', 'https://www.kraken.com/'),
-('bitmap', 'https://www.kraken.com/'),
-('coinsquare', 'https://www.kraken.com/');
+('bitbuy', 'https://www.bitbuy.com/'),
+('bitmap', 'https://www.bitmap.com/'),
+('coinsquare', 'https://www.coinsquare.com/');
