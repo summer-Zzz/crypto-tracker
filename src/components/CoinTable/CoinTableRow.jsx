fe68c934c72019f51032cdd646dca42c37fa6349
summer-Zzz <summerellaooo@gmail.com>
2021-05-20 03:06:29 +0800

    add data and dollarsign

--- src/components/CoinTable/CoinTableRow.jsx
+++ src/components/CoinTable/CoinTableRow.jsx
@@ -2,14 +2,21 @@
 import "./CoinTableRow.scss"
 
 export default function CoinTableRow(props) {
-console.log('coinlogo', props.coinLogo)
+  const addDollarSign = (num) => {
+    if (num < 0) {
+      return `-$${-num.toFixed(2)}`;
+    } else {
+      return `$${num.toFixed(2)}`;
+    }
+  }
+
   return (
     <tr onClick={() => props.setCoin(props.symbol)}>
       <td className="coin-td"><img className='coin-logo' src={props.coinLogo} alt="coinlogo"/></td>
       <td className="coin-column">{props.symbol}</td>
-      <td className="price">{props.price}</td>
-      <td className="coin-td">{props.change}</td>
-      <td className="coin-td">{props.changePercent}</td>
+      <td className="price">${props.price.toFixed(2)}</td>
+      <td className="coin-td">{addDollarSign(props.change)}</td>
+      <td className="coin-td">{props.changePercent}%</td>
       <td className="coin-td">{props.volume.toFixed(2)}</td>
       <td><button className="coin-button" onClick={() => props.setCoin(props.symbol)}>Select Coin</button></td>
     </tr>
