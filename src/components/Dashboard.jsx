62b96d52f151ab9fb3cff599264a81d4f91bcca9
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-05-19 10:22:33 -0400

    re-name month time period

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -41,6 +41,14 @@ const totalCost = (trades, currentCoin) => {
   return priceTotal.toFixed(2);
 }
 
+const checkPl = (pL) => {
+  if(pL.includes('+')) {
+    console.log(pL)
+    return true
+  }
+}
+
+
 const time = [
   {
     id: 1,
@@ -92,13 +100,6 @@ export default function Dashboard(props) {
   const pL = calculatePL(trades, coin);
   const average = `$${averageCost(trades, coin)}`;
   const total = `$${totalCost(trades, coin)}`
-
-  const checkPl = (pL) => {
-    if(pL.includes('+')) {
-      console.log(pL)
-      return true
-    }
-  }
 
   return (
     <div className='dashboard-container'>
