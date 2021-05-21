bbd5d9934a992281a1730e6c4f8f1cddfc8ce1b3
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-05-21 11:18:56 -0400

    remove cookiesession from backend

--- express-server/app.js
+++ express-server/app.js
@@ -4,7 +4,6 @@ const logger = require('morgan');
 const db = require('./db/index');
 const cors = require('cors');
 require('dotenv').config();
-const cookieSession = require('cookie-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,11 +14,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}))
- 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
